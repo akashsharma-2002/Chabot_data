bf781a60e8fbc9482e472f4f9078dfbd21889300
Akash Sharma <akashbest2002@gmail.com>
2025-09-01 19:23:12 +0530

    pushing

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Chabot_data',
+    version='0.1',
+    author='Akash sharma',
+    author_email='akashbest2002@gmail.com',
+    description='A chatbot made for data scientist',
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ template.sh
@@ -0,0 +1,15 @@
+#creating directory structure for the code.
+mkdir -p src
+mkdir -p research
+
+#creating file inside directory
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory structure created."
